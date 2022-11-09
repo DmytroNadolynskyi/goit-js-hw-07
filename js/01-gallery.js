e987b7325af52e8fd5d62841857a2b00477ac3cf
DmytroNadolynskyi <113470234+DmytroNadolynskyi@users.noreply.github.com>
2022-11-09 11:32:52 +0200

    hw-js-07-onShow-onClose

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -21,22 +21,50 @@ galleryList.insertAdjacentHTML("beforeend", galleryImage);
 
 const galleryItem = document.querySelector("div.gallery");
 
-galleryItem.addEventListener("click", e => {
-    e.preventDefault();
+// galleryItem.addEventListener("click", e => {
+//     e.preventDefault();
 
-    if (e.target.nodeName !== "IMG") {
-        return;
-    };
+//     if (e.target.nodeName !== "IMG") {
+//         return;
+//     };
 
-    const modalDataAttribute = e.target.getAttribute("data-source");
-    const instance = basicLightbox.create(
-        `<img src="${modalDataAttribute}">`
-    );
-    instance.show();
+//     const modalDataAttribute = e.target.getAttribute("data-source");
+//     const instance = basicLightbox.create(
+//         `<img src="${modalDataAttribute}">` );
+//     instance.show();
 
-    document.addEventListener("keydown", e => {
-        if (e.code === "Escape") {
-            instance.close();
-        };
-    });
-});
+//     document.addEventListener("keydown", e => {
+//         if (e.code === "Escape") {
+//             instance.close();
+//         };
+//     });
+// });
+galleryItem.addEventListener('click', imageModalOpen);
+
+function imageModalOpen(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  makeBLB(e.target);
+}
+
+function makeBLB(el) {
+  const instance = basicLightbox.create(`<img src="${el.dataset.source}">`, {
+    onShow: instance => {
+      document.addEventListener('keydown',  escapeCloseModal.bind(instance));
+    },
+    onClose: instance => {
+      document.removeEventListener('keydown',  escapeCloseModal.bind(instance));
+    },
+  });
+
+  instance.show();
+}
+
+function escapeCloseModal(e) {
+  if (e.code === 'Escape') {
+   this.close();
+  }
+};
+
